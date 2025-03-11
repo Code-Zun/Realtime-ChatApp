e1d5d10ac3fe0f76cf3e6e13dc08f2d76eed433f
Rivaldo Soebianto <96009065+Code-Zun@users.noreply.github.com>
2025-03-12 00:40:55 +0800

    User DB Schema Model

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,19 @@
-const express = require("express")
+import express from "express"; 
+import dotenv from "dotenv"
+import {connectDB} from "./lib/db.js"; 
+dotenv.config();
+
+//web framework to help build API's
+
+import authRoutes from "./routes/auth.route.js";
 
 const app = express(); 
+const PORT = process.env.PORT;
 
-app.listen(5001, () => { 
-    console.log("Server is running on port 5001")
-})
+app.use(express.json); 
+app.use("/api/auth", authRoutes); 
+
+app.listen(PORT, () => { 
+    console.log("Server is running on PORT:" + PORT);
+    connectDB(); 
+}); 
